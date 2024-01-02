359bb05cd7c9a26cd33e701d00fd972e8c2d386c
William Thompson <wmthmps1@gmail.com>
2024-01-01 20:56:17 -0600

    Cleaned up comments

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -11,7 +11,7 @@
         {
             logger.LogInfo("Begin parsing");
 
-            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
+            
             var cells = line.Split(',');
 
             // If your array's Length is less than 3, something went wrong
@@ -21,36 +21,18 @@
                 return null; 
             }
 
-            // TODO: Grab the latitude from your array at index 0
-            // You're going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
+            
             double lat = double.Parse(cells[0]);
-
-            // TODO: Grab the longitude from your array at index 1
-            // You're going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
             double lon = double.Parse(cells[1]);
-
-            // TODO: Grab the name from your array at index 2
             string name = cells[2];
 
-            // TODO: Create a TacoBell class
-            // that conforms to ITrackable
-
-            // TODO: Create an instance of the Point Struct
-            // TODO: Set the values of the point correctly (Latitude and Longitude)
             var point = new Point();
             point.Latitude = lat;
             point.Longitude = lon;
 
-            // TODO: Create an instance of the TacoBell class
-            // TODO: Set the values of the class correctly (Name and Location)
             var tacoBell = new TacoBell();
             tacoBell.Name = name;
             tacoBell.Location = point;
-
-            // TODO: Then, return the instance of your TacoBell class,
-            // since it conforms to ITrackable
 
             return tacoBell;
         }
